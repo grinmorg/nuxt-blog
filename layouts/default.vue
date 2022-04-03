1eb67bc721470043fb1024d2bd695f1ccb7446f0
Grigory Morgachev <grinmorg@gmail.com>
2022-04-03 12:26:54 +0300

    init

---
+++ layouts/default.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app>
+    <TheNavigation />
+    <v-main>
+      <v-container>
+        <v-row>
+          <v-col cols="12" md="10">
+            <Nuxt />
+          </v-col>
+          <v-col cols="12" md="2">
+            <!-- Ads / permanent side content -->
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout',
+}
+</script>
+
+<style></style>
